fff033b9d8f9b3d4bea0a56366d96f0af6b0f11b
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-01-14 19:17:07 +0100

    feat: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,65 @@ import GlobalContext from "./contexts/GlobalContext"
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-const apiUrl = "https://api.themoviedb.org/3/search/movie"
-const key = "febf50335e1f2492dcf7e91bd2f58719"
+const apiUrl = "https://api.themoviedb.org/3/search"
+const apiKey = "febf50335e1f2492dcf7e91bd2f58719"
 
 function App() {
 
-  const [search, setSearch] = useState([]);
+  const [searchValue,setSearchValue] =useState("");
+  const [Movie, setMovie] = useState([]);
+  
 
-  useEffect(() => {
-
-    getMovies();
-
-  }, [])
-
+  //prende i film
   const getMovies = () => {
-
-    axios.get(`${apiUrl}`, {
+    axios.get(`${apiUrl}/movie`, {
       params: {
-        api_key: key,
-        query: "batman"
+        api_key: apiKey,
+        query: searchValue,
+        language: "it-IT"
       }
     }).then((resp) => {
-      setSearch(resp.data.results)
+      setMovie(resp.data.results)
+      console.log(resp.data.results);
       
     });
   }
 
-  
-  
 
 
   const globalProviderValue = {
-    search,
-    setSearch,
+    Movie,
+    setMovie,
+    
   }
+
 
   return (
     <>
       <GlobalContext.Provider value={globalProviderValue}>
         <div>
-          {search.map((curMovie,index) => {
+          <input type="search" value={searchValue} onChange={(event) => setSearchValue(event.target.value)}/>
+          <button onClick={() => {getMovies()}}>Cerca</button>
+          <br />
 
+          <h2>Film</h2>
+          {globalProviderValue.Movie.map((curMovie, index) => {
             return (
               <div key={index}>
-                {curMovie.title}
+                <h3>Titolo</h3> 
+                <div>{curMovie.title}</div>
+                <h3>Titolo Originale </h3>
+                <div>{curMovie.original_title}</div> 
+                <div><strong>lingua </strong>: {curMovie.original_language}</div>
+                <div><strong>voto :</strong> {curMovie.vote_average}</div>
+                <br />
               </div>
             )
           })}
+
+          
         </div>
-        
+
       </GlobalContext.Provider>
     </>
   )
